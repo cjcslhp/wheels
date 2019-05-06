791ac67b4639a08fca6d756e29fade1ec904d857
cjcslhp <bendelingyike@163.com>
2019-05-06 15:21:56 +0800

    update common mode

--- rsa/common_mode_attack/common_mode_attack.py
+++ rsa/common_mode_attack/common_mode_attack.py
@@ -1,21 +1,29 @@
+from Crypto.Util.number import long_to_bytes
 import gmpy
 
+def common_mode_attack(n, e1, e2, c1, c2):
+    print("[*] Try common mode attack")
+    _, s1, s2 = gmpy.gcdext(e1, e2)
+    if s1 < 0:
+        s1 = - s1
+        c1 = gmpy.invert(c1, n)
+    elif s2 < 0:
+        s2 = - s2
+        c2 = gmpy.invert(c2, n)
 
-n = int(input("n:"))
-c1 = int(input("c1:"))
-c2 = int(input("c2:"))
-e1 = int(input("e1:"))
-e2 = int(input("e2:"))
+    m = (pow(c1,s1,n)*pow(c2,s2,n)) % n
+    print("[*] Maybe success")
+    print("[!] flag:",long_to_bytes(m))
+    return 1
 
-_, s1, s2 = gmpy.gcdext(e1, e2)
+def main(n,e1,e2,c1,c2):
+    if common_mode_attack(n, e1, e2, c1, c2):
+        print("[-] Thanks, bye~")
+        return
 
-if s1 < 0:
-    s1 = - s1
-    c1 = gmpy.invert(c1, n)
-elif s2 < 0:
-    s2 = - s2
-    c2 = gmpy.invert(c2, n)
+    print("[-] Sorry, it's not easy~")
+    return
 
-m = (pow(c1,s1,n)*pow(c2,s2,n)) % n
-
-print(m)
+if __name__ == "__main__":
+    exec(open("nec.txt").read())
+    main(n, e1, e2, c1, c2)
